cd1050daf8fce8fa5481c29590a33ea3063d70a0
Rincevent42 <porvooc@gmail.com>
2020-07-02 14:39:27 +0200

    Email - corrections suivant Pierre

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000)
 app.use(express.urlencoded({ extended: true }));
 
 // middlewares
-app.use((req, res, next) => {
-  console.log('loggggggggggggggggg');
-  next();
-});
-
 app.use(express.json());
 app.use(cors());
 if (process.env.NODE_ENV !== 'production') {
