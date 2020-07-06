370e01681d808d6baa8149d63446791c01ad1845
Armin <arminit3@gmail.com>
2020-07-07 00:23:02 +0200

    update

--- controllers/freelances.controller.js
+++ controllers/freelances.controller.js
@@ -36,10 +36,10 @@ class FreelancesController {
 
   static async findOne (req, res) {
     try {
-      const freelances = await Freelance.findById(req.params.id);
+      const freelance = await Freelance.findById(req.params.id);
       const tags = await Freelance.getAllTags(req.params.id);
       const references = await Freelance.getAllReferences(req.params.id);
-      res.send({ freelances, tags, references });
+      res.send({ freelance, tags, references });
     } catch (err) {
       if (err.kind === 'not_found') {
         res.status(404).send({ errorMessage: `Freelance with id ${req.params.id} not found.` });
