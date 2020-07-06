6a7a1b66592b0c6b6effcf68713feec4d60b3971
Christophe CRÉBIER <60516494+Rincevent42@users.noreply.github.com>
2020-07-06 14:48:39 +0200

    Feature/validation (#23)
    * update DB
    
    * routes selon plan test
    
    * update DB
    
    * is good for user route
    
    * update
    
    * is good for freelance route
    
    * is all good for user route
    
    * package.json
    
    * update
    
    * add test files
    
    * update
    
    * fix some problem
    
    * update
    
    * First user test done
    
    * POST tests 2 and 3 added
    
    * POST tests on going
    
    * POST tests on going - correction
    
    * Email validation - fonctionnelle
    
    * Email validation - cycle fonctionnel création du compte + validation
    
    * Email validation - ajout is_validated:0 lors de la création du compte
    
    * Email validation - meilleure gestion des erreurs
    
    * Email validation - sauvegarde le key dans la table
    
    * Email validation - sauvegarde le key dans la table
    
    * Email validation - lien vers page connexion
    
    * Email validation - corrections sur les tests
    
    * Email validation - corrections sur les tests
    
    * Email validation - corrections sur les tests
    
    * .env.sample
    
    * Validation - URL dans .env et test sur délai de 48h max pour validation
    
    * Validation - vérification du délai de réponse limité à 2 jours
    
    * Validation - corrections diverses
    
    * Validation - utilisation du champ is_validated pour le statut de la validation
    
    * Validation - retour en arrière pour passage d'info via URL
    
    * Validation - correction bug
    
    * Validation - ajout du traitement du renvoi d'email de validation
    
    * Validation Front - renvoi d'email à la demande du freelance câblé et fonctionnel
    
    * Validation - modifications des routes /:id par ?id=
    
    * Validation - envoi d'emails par gmail
    
    Co-authored-by: wdwcs <arminit3@gmail.com>
    Co-authored-by: Pascal-Pereira <pereira.pascal@gmail.com>

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,12 @@ const usersController = require('../controllers/users.controller.js');
 const authController = require('../controllers/auth.controller.js');
 const router = require('express').Router();
 
+// Validation d'un nouveau freelance par son email
+router.get('/validation_email', usersController.validationByEmail);
+
+// Lorsque le freelance demande le renvoi d'un email de validation
+router.post('/renvoi_email_validation', usersController.resendValidationEmail);
+
 router.post('/', usersController.create);
 router.get('/', usersController.findAll);
 router.get('/:id', usersController.findOne);
