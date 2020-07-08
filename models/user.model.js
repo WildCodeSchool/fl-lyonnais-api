66aa2510a70f7d4e138b00c3e1c205c91ebc88ba
Armin <arminit3@gmail.com>
2020-07-08 20:34:35 +0200

    update

--- models/user.model.js
+++ models/user.model.js
@@ -7,8 +7,7 @@ class User {
   static async create (newUser) {
     const hash = await argon2.hash(newUser.password);
     const addUser = { ...newUser, password: hash };
-    return db.query('INSERT INTO user (email, firstname, lastname, password, siret, is_validated, registration_date, `key`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-      [addUser.email, addUser.firstname, addUser.lastname, addUser.password, addUser.siret, addUser.is_validated, addUser.registration_date, addUser.key])
+    return db.query('INSERT INTO user SET ?', addUser)
       .then(res => {
         newUser.id = res.insertId;
         const { password, ...addUser } = newUser;
@@ -59,8 +58,8 @@ class User {
 
   static async updateById (id, user) {
     return db.query(
-      'UPDATE user SET email = ?, firstname = ?, lastname = ?, siret = ?, is_validated = ?, last_connection_date = ? WHERE id = ?',
-      [user.email, user.firstname, user.lastname, user.siret, user.is_validated, user.lastConnectionDate, id]
+      'UPDATE user SET email = ?, firstname = ?, lastname = ?, siret = ?, is_validated = ? WHERE id = ?',
+      [user.email, user.firstname, user.lastname, user.siret, user.is_validated, id]
     ).then(() => this.findById(id));
   }
 
