fbb45b1aba0844b621f503037376c2c8d94fb983
Christophe <porvooc@gmail.com>
2020-07-08 19:08:54 +0200

    last connection date - ajout du champ key perdu dans la modification dans la requête de création

--- models/user.model.js
+++ models/user.model.js
@@ -7,8 +7,8 @@ class User {
   static async create (newUser) {
     const hash = await argon2.hash(newUser.password);
     const addUser = { ...newUser, password: hash };
-    return db.query('INSERT INTO user SET email = ?, firstname = ?, lastname = ?, password = ?, siret = ?, is_validated = ?, registration_date = ?',
-      [addUser.email, addUser.firstname, addUser.lastname, addUser.password, addUser.siret, addUser.is_validated, addUser.registration_date])
+    return db.query('INSERT INTO user SET email = ?, firstname = ?, lastname = ?, password = ?, siret = ?, is_validated = ?, registration_date = ?, key = ?',
+      [addUser.email, addUser.firstname, addUser.lastname, addUser.password, addUser.siret, addUser.is_validated, addUser.registration_date, addUser.key])
       .then(res => {
         newUser.id = res.insertId;
         const { password, ...addUser } = newUser;
