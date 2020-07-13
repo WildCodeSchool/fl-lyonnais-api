2b01b528725df95fa13d965995d7984e1402b0a4
Christophe <porvooc@gmail.com>
2020-07-13 16:46:54 +0200

    Search - renommage des fichiers de tests car bloquent la PR

--- controllers/searches.controller.js
+++ controllers/searches.controller.js
@@ -2,11 +2,17 @@ const Search = require('../models/search.model.js');
 
 class SearchController {
   static async search (req, res) {
-    const searchItems = req.query.recherche;
-    const searchResults = await Search.search(searchItems);
-    console.log(searchResults.length);
-    console.log(searchResults);
-    return searchResults;
+    try {
+      const searchItems = req.query.recherche;
+      const searchResults = await Search.search(searchItems);
+      console.log(searchResults.length);
+      console.log(searchResults);
+      res.send({ searchResults });
+    } catch (err) {
+      res.status(500).send({
+        errorMessage: err.message || "La recherche n'a pas abouti."
+      });
+    }
   }
 }
 
