d13f70b2be0b6faf64775b753895d53d6810134b
Armin <arminit3@gmail.com>
2020-07-14 02:46:44 +0200

    add url_photo in search

--- models/search.model.js
+++ models/search.model.js
@@ -4,7 +4,7 @@ class Search {
   static async search (searchItems) {
     const searchItemsTable = searchItems.split(';');
 
-    const queryCommon = 'SELECT freelance.id AS freelance_id, user.firstname, user.lastname, freelance.job_title FROM freelance JOIN user ON freelance.user_id = user.id WHERE freelance.user_id IN (SELECT DISTINCT freelance.user_id FROM freelance LEFT JOIN user ON freelance.user_id = user.id LEFT JOIN freelance_tag ON freelance_tag.freelance_id = freelance.id LEFT JOIN tag ON freelance_tag.tag_id = tag.id WHERE ';
+    const queryCommon = 'SELECT freelance.id AS freelance_id, user.firstname, user.lastname, freelance.job_title, freelance.url_photo FROM freelance JOIN user ON freelance.user_id = user.id WHERE freelance.user_id IN (SELECT DISTINCT freelance.user_id FROM freelance LEFT JOIN user ON freelance.user_id = user.id LEFT JOIN freelance_tag ON freelance_tag.freelance_id = freelance.id LEFT JOIN tag ON freelance_tag.tag_id = tag.id WHERE ';
 
     let queryWhere = '';
     const queryEscapeTable = [];
